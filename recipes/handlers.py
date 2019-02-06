c222985defa341b193ddfb2a4b03cf48afd467ac
Osvaldo Santana Neto <osantana@osantana.me>
2019-02-06 02:18:27 -0200

    Add first RecipesApp's Resources

--- recipes/handlers.py
+++ recipes/handlers.py
@@ -1,16 +1,20 @@
 from toy.handlers import ResourceHandler
+from .resources import RatingResource, RecipeResource, RecipesResource
 
 MAX_PAGE_SIZE = 50
 
 
 class Recipes(ResourceHandler):
     route_template = '/recipes'
+    resource_type = RecipesResource
 
 
 class Recipe(ResourceHandler):
     allowed_methods = ['post']
     route_template = '/recipes/<id>'
+    resource_type = RecipeResource
 
 
 class Rating(ResourceHandler):
     route_template = '/recipes/<id>/rating'
+    resource_type = RatingResource
---
+++ recipes/resources.py
@@ -0,0 +1,24 @@
+from toy import fields
+from toy.resources import Resource
+
+
+class RatingResource(Resource):
+    fields = [
+        fields.UUIDField(name='id', required=True, lazy=True),
+        fields.IntegerField(name='value', min_value=1, max_value=5),
+    ]
+
+
+class RecipeResource(Resource):
+    fields = [
+        fields.UUIDField(name='id', required=True, lazy=True),
+        fields.CharField(name='name', max_length='255', required=True),
+        fields.IntegerField(name='prep_time', min_value=0, required=True),
+        fields.IntegerField(name='difficulty', min_value=1, max_value=3, required=True),
+        fields.BooleanField(name='vegetarian', required=True),
+        fields.ResourceListField(name='ratings', resource_type=RatingResource),
+    ]
+
+
+class RecipesResource(Resource):
+    pass  # TODO
