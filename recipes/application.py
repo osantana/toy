00cd21cfc496de7329a831882b2b1c595d203114
Osvaldo Santana Neto <osantana@osantana.me>
2019-02-08 00:58:23 -0200

    Fix routes mapping to avoid ambiguity

--- recipes/application.py
+++ recipes/application.py
@@ -11,10 +11,10 @@ class RecipesApp(Application):
         self.config.setdefault('database_url', config('DATABASE_URL'))
 
         recipe_handler = handlers.Recipe(application=self)
-        self.add_route(r'/recipes', handlers.Recipes(application=self))  # GET only
-        self.add_route(r'/recipes', recipe_handler)  # POST only
-        self.add_route(r'/recipes/(?P<id>\d+)', recipe_handler)
-        self.add_route(r'/recipes/(?P<id>\d+)/rating', handlers.Rating(application=self))
+        self.add_route(r'^/recipes$', handlers.Recipes(application=self))  # GET only
+        self.add_route(r'^/recipes$', recipe_handler)  # POST only
+        self.add_route(r'^/recipes/(?P<id>[0-9a-f-]+)$', recipe_handler)
+        self.add_route(r'^/recipes/(?P<id>[0-9a-f-]+)/rating$', handlers.Rating(application=self))
 
 
 def get_app(**kwargs):
